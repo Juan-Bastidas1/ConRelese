1f4ccd61b4f2bf9307ab2da07d0a065aa57ff81a
Juan-Bastidas1 <lisofreal@gmail.com>
2025-02-07 21:15:40 -0500

    primr lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CONRelese",                               # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Juan Bastidas",                         # Tu nombre
+    author_email="lisofreal@gmail.com",             # Tu correo electrónico
+    url="https://github.com/Juan-Bastidas1/CONRelese", # URL del proyecto
+)
